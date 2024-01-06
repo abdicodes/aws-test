cf7d8e99f03e230e38e54470fe48992d573931d1
Abdirahman Suldaan <suldaan.89@hotmail.com>
2024-01-06 09:02:50 +0200

    init stairsTransition

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local'
 import './globals.css'
 import Header from '@/components/ui/Header'
 import PageTransition from '@/components/ui/PageTransition'
+import StairTransition from '@/components/ui/StairTransition'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+        <StairTransition />
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
