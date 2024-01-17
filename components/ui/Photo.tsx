961ffae6e8e7c0e8a7a5620fd9cd813c3219d537
Abdirahman Suldaan <suldaan.89@hotmail.com>
2024-01-17 23:29:54 +0200

    implemented rotating circle frame for photo

--- components/ui/Photo.tsx
+++ components/ui/Photo.tsx
@@ -14,14 +14,14 @@ const Photo = () => {
           transition: { delay: 2, duration: 0.4, ease: 'easeIn' },
         }}
       >
+        {/* image */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: 'easeInOut' },
           }}
-          className="w-[298px] h-[108px] xl:w-[498px] xl:h-[498px]
-        "
+          className="w-[298px] h-[198px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
             src="/images/photo.png"
@@ -32,6 +32,33 @@ const Photo = () => {
             fill
           />
         </motion.div>
+        {/* circle */}
+        <motion.svg
+          className="w-[300px] xl:w-[506px] h-[200px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            initial={{ strokeDasharray: '24 10 0 0' }}
+            animate={{
+              strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
+              rotate: [120, 360],
+            }}
+            transition={{
+              delay: 1.5,
+              duration: 20,
+              repeat: Infinity,
+              repeatType: 'reverse',
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   )
