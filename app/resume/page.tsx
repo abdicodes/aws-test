d76a70825820c5e3aab00e82aeecfa5263618f7c
Abdirahman Suldaan <suldaan.89@hotmail.com>
2024-01-22 16:32:38 +0200

    implemented resume page and experience section

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -68,17 +68,17 @@ const experience = {
     {
       company: 'Abdi trading Oy',
       position: 'Entreprenuer',
-      duration: '2022-present',
+      duration: '2022 - present',
     },
     {
       company: 'Deepcode Innovations Ltd',
       position: 'Trainee web developer',
-      duration: '02-2023-08-2023',
+      duration: '02-2023 - 08-2023',
     },
     {
       company: 'Posti Oyj',
       position: 'Mail hanlding',
-      duration: '2017-2021',
+      duration: '2017 - 2021',
     },
   ],
 }
@@ -150,7 +150,47 @@ const Resume = () => {
           {/* content */}
           <div className="min-h-[70vh] w-full ">
             <TabsContent value="experience" className="w-full">
-              experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 mx-4
+                        rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent">{item.duration}</span>
+                          <h3
+                            className="text-xl max-w-[260px] min-h-[60px]
+                          text-center lg:text-left"
+                          >
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            {/* dot */}
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60">{item.company}</p>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              education
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              about
             </TabsContent>
           </div>
         </Tabs>
