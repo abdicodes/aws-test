e132dca913ac7eb78aaaa1a344e7a595acdc4c9d
Abdirahman Suldaan <suldaan.89@hotmail.com>
2024-01-23 13:06:12 +0200

    styled skills section

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -234,9 +234,24 @@ const Resume = () => {
                     {skills.decsription}
                   </p>
                 </div>
-                <ul className="grid ">
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
                   {skills.skillsList.map((skill, index) => {
-                    return <li key={index}>{skill.name}</li>
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl group-hover:text-accent  transition-all duration-300">
+                                {skill.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className="capitalize"> {skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    )
                   })}
                 </ul>
               </div>
