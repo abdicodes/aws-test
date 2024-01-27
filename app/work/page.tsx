32f9e564ff1920b8eaf911ce3efc1573d644fc63
Abdirahman Suldaan <suldaan.89@hotmail.com>
2024-01-27 12:42:36 +0200

    implemented thumb slides in work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react'
 import { motion } from 'framer-motion'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import { BsArrowUpRight, BsGithub } from 'react-icons/bs'
 import {
@@ -49,10 +49,18 @@ const projects = [
 ]
 const Work = () => {
   const [project, setProject] = useState(projects[0])
+
+  const handleSlideChange = (swiper: { activeIndex: number }) => {
+    const currentIndex = swiper.activeIndex
+    setProject(projects[currentIndex])
+  }
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' },
+      }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
@@ -131,7 +139,37 @@ const Work = () => {
               </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%]">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index}>
+                    <div
+                      className="h-[460px] relative group flex justify-center 
+                    items-center bg-pink-50/20 "
+                    >
+                      {/* overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black"></div>
+                      {/* image */}
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                )
+              })}
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
